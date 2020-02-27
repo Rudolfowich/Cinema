80a3eec2a12c8e5eb6cea4fb7d5fe77e494b279a
Ilya <rudich.official@gmail.comgit config --global user.name Your>
2020-02-27 15:59:10 +0200

    fix gitignore

--- authorization/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from authorization import views
-
-urlpatterns = [
-    path('login/', views.Login.as_view(), name='login'),  # Урл на логин
-    path('logout/', views.LogoutView.as_view(), name='logout'),  # Урл на логаут
-    path('register/', views.RegisterCreateView.as_view(), name='register'),  # Урл на Регистрацию
-]
